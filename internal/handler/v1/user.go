d3a3d10f1bfc01ff90cc2ba39a5d6effc711dd10
danisbagus <danisbagussetiawan@gmail.com>
2022-01-26 15:08:09 +0700

    enhance to write json response

--- internal/handler/v1/user.go
+++ internal/handler/v1/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/danisbagus/go-common-packages/http/response"
 	"github.com/danisbagus/go-common-packages/logger"
 	"github.com/danisbagus/matchoshop/internal/core/port"
 	"github.com/danisbagus/matchoshop/internal/dto"
@@ -17,40 +18,31 @@ func (rc AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	var loginRequest dto.LoginRequest
 	if err := json.NewDecoder(r.Body).Decode(&loginRequest); err != nil {
 		logger.Error("Error while decoding login request: " + err.Error())
-		w.WriteHeader(http.StatusBadRequest)
+		response.Write(w, http.StatusBadRequest, "Failed to login")
 		return
 	}
 
 	token, appErr := rc.Service.Login(loginRequest)
 	if appErr != nil {
-		writeResponse(w, appErr.Code, appErr.AsMessage())
+		response.Write(w, appErr.Code, appErr.AsMessage())
 		return
 	}
 
-	writeResponse(w, http.StatusOK, *token)
+	response.Write(w, http.StatusOK, *token)
 }
 
 func (rc AuthHandler) RegisterMerchant(w http.ResponseWriter, r *http.Request) {
 	var registerRequest dto.RegisterMerchantRequest
 	if err := json.NewDecoder(r.Body).Decode(&registerRequest); err != nil {
 		logger.Error("Error while decoding register merchant request: " + err.Error())
-		w.WriteHeader(http.StatusBadRequest)
+		response.Write(w, http.StatusBadRequest, "Failed to register merchant")
 		return
 	}
 
 	token, appErr := rc.Service.RegisterMerchant(&registerRequest)
 	if appErr != nil {
-		writeResponse(w, appErr.Code, appErr.AsMessage())
+		response.Write(w, appErr.Code, appErr.AsMessage())
 		return
 	}
-	writeResponse(w, http.StatusOK, *token)
-
-}
-
-func writeResponse(w http.ResponseWriter, code int, data interface{}) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(code)
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		panic(err)
-	}
+	response.Write(w, http.StatusOK, *token)
 }
