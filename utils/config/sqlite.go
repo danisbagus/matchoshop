994a5a59c9d35bc396c69d608afe27e75b783925
Danis Bagus Setiawan <danis.bagus@majoo.id>
2023-03-25 04:16:37 +0700

    Revert "enhance init sqlite"
    This reverts commit bb5335f44fd9499ced79580b0af475996821b94c.

--- utils/config/sqlite.go
+++ utils/config/sqlite.go
@@ -3,6 +3,7 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,7 +12,7 @@ var db *sql.DB
 
 func InitSqlite() {
 	var err error
-	file := GetDBFile()
+	file := os.Getenv("DB_FILE")
 
 	db, err = sql.Open("sqlite3", file)
 	if err != nil {
